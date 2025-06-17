b19fe8c4d2d6760a4118c2e80f52a2ab41231f54
abdullahsher <abdullahhswat1234@gmail.com>
2025-06-17 13:05:27 +0500

    First Challange done.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ function App() {
   return (
     <div>
       <h1>This is just a paragraph.</h1>
+      <ul>
+        <li>Li 1</li>
+        <li>Li 2</li>
+        <li>Li 3</li>
+      </ul>
     </div>
   )
 }
---
+++ src/components/newcomponenet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function newcomponenet() {
+  return (
+    <div>
+      <h1>React Is Great!</h1>
+    </div>
+  )
+}
+
+export default newcomponenet
