c2c25fdb865491b3a6889713508aad0a7a754b22
abdullahsher <abdullahhswat1234@gmail.com>
2025-06-17 16:10:29 +0500

    Added another componenet component2.

---
+++ src/components/newcomponenets2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function NewComponent2() {
+  return (
+    <div id="root">
+      <h1 className="header">Welcome to My Website</h1>
+    </div>
+  );
+}
+
+export default NewComponent2;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import NewComponenet from './components/newcomponenet.jsx'
+import NewComponenets2 from './components/newcomponenets2.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
     <App />
     <NewComponenet/>
+    <NewComponenets2/>
   </StrictMode>
 )
